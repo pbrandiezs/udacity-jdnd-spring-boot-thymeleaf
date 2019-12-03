bb81e7fe24da6ce142485f4d6ed28ee7d23e1e47
Perry Brandiezs <pbrandiezs@gmail.com>
2019-12-03 08:50:54 -0800

    feat: Added demo2 mapping

--- src/main/java/com/example/springbootthymeleaf/controller/UserController.java
+++ src/main/java/com/example/springbootthymeleaf/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.example.springbootthymeleaf.controller;
 
+import com.example.springbootthymeleaf.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -30,5 +34,15 @@ public class UserController {
         } else {
             return "F";
         }
+    }
+
+    @RequestMapping("demo2")
+    public String demo2(Model model) {
+        List<User> lst = new ArrayList<>();
+        lst.add(new User(1, "Tom", 30));
+        lst.add(new User(2, "Jerry", 29));
+        lst.add(new User(3, "Nancy", 27));
+        model.addAttribute("list", lst);
+        return "demo2";
     }
 }
